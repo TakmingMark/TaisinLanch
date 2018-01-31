e2f19e074eb7445b773bb4ecdc880ec821b292cc
MarkWang <nark29880420@gmail.com>
2018-01-31 09:41:16 +0800

    Start insert ingredient unit

---
+++ src/Component/IngredientComponent.java
@@ -0,0 +1,19 @@
+package Component;
+
+public class IngredientComponent {
+private String name;
+private String unit;
+public String getName() {
+	return name;
+}
+public String getUnit() {
+	return unit;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public void setUnit(String unit) {
+	this.unit = unit;
+}
+
+}
